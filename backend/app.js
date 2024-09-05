4d744210ad0fa9c9b22a5f31f8924fceaa3edded
kaustubhduse <202251045@iiitvadodara.ac.in>
2024-09-05 16:44:35 +0530

    Describe the changes you made

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,9 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://hackathon-lyart-one.vercel.app',
+  }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", CardRoute);
