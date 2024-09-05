c5e1bb2d252fa3e26a4ce8fb0c6ba7e139ab45ac
kaustubhduse <202251045@iiitvadodara.ac.in>
2024-09-05 18:48:31 +0530

    Describe the changes you made

--- src/components/UI/StaticCard.js
+++ src/components/UI/StaticCard.js
@@ -1,14 +1,13 @@
-// StaticCard.js
-import React from 'react';
+import React from 'react'
 
-function StaticCard({ img, alt, title, description }) {
+function StaticCard(props) {
   return (
-    <div className="static-card bg-white p-6 rounded-lg shadow-md text-center">
-      <img src={img} alt={alt} className="mx-auto mb-4 w-20 h-20" />
-      <h2 className="text-xl font-semibold mb-2">{title}</h2>
-      <p className="text-gray-600">{description}</p>
+    <div className='static-card lg:w-[80%] text-left bg-[rgba(248,249,253,1)] rounded-xl space-y-4 justify-center p-6'>
+       <img src={props.img} alt={props.alt} className="" />
+       <h1 className='font-bold text-2xl'>{props.title}</h1>
+       <p className='text-[rgba(100,96,125,1)]'>{props.description}</p>
     </div>
-  );
+  )
 }
 
-export default StaticCard;
+export default StaticCard
